e84a4c15484fdb6a5075102f78ad5dc4277b61da
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-05 14:20:39 +0700

    Refactor icon usage in overview components by replacing image tags with SVGs and removing unused icon files

--- src/features/overview/GraphCard.jsx
+++ src/features/overview/GraphCard.jsx
@@ -6,7 +6,9 @@ export default function GraphCard({ title, graphComponent }) {
     <div className="graph-card">
       <div className="head">
         <h4>{title}</h4>
-        <img src="/assets/icons/three-dots.svg" alt="more" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+          <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+        </svg>
       </div>
       <div className="graph">
         {graphComponent ? graphComponent : <p>No graph available</p>}
--- src/features/overview/PerformanceOverTime.jsx
+++ src/features/overview/PerformanceOverTime.jsx
@@ -7,8 +7,13 @@ export default function PerformanceOverTime({ data }) {
       <div className="head">
         <h4>Revenue Over Time</h4>
         <div className="actions">
-          <img src="\assets\icons\download.svg" alt="download" />
-          <img src="\assets\icons\three-dots.svg" alt="more" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+          </svg>
         </div>
       </div>
       <div className="info">
