7c37c4eb8c3bf05e233e68cb60dfaed5bfed4dd3
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-26 12:06:02 +0700

    feat: add OverViewSession component with styling for session overview

---
+++ src/features/overview/OverViewSession.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./OverViewSession.css";
+
+export default function OverViewSession({ title, subtitle, overviewSession }) {
+  return (
+    <div className="session-container">
+      <div className="session-title">
+        <h4>{title}</h4>
+        <p>{subtitle}</p>
+      </div>
+      <div className="session-content">
+        {overviewSession.map((customer, index) => (
+          <div className="widget" key={index}>
+            <div className="head">
+              <h6 title={customer.name}>{customer.name}</h6>
+              <h6>
+                {customer.sessions} • {customer.percentage.toFixed(2)}%
+              </h6>
+            </div>
+            <div className="bar">
+              <div
+                className="active"
+                style={{ width: `${customer.percentage}%` }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
