98f4d234593f49152a4ee5c117145df215f852ef
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-26 14:02:37 +0700

    feat: add OverviewTable component for displaying overview data

---
+++ src/features/overview/OverviewTable.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./OverviewTable.css";
+
+export default function OverviewTable({ title }) {
+  return (
+    <div className="overview-table">
+      <h5>{title}</h5>
+      <div className="table-wrapper">
+        {/* table here */}
+
+        {/* table here */}
+      </div>
+    </div>
+  );
+}
