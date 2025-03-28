e157c20e229b31dbc474f812f20b29f5fea7b247
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-29 01:50:30 +0700

    add responsive header with menu click functionality and logo display

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import "./Header.css";
 
-export default function Header({ title, user }) {
+export default function Header({ title, user, onMenuClick }) {
   const location = useLocation();
 
   return (
     <div className="header-container">
-      <h5 className="title">{title}</h5>
+      <div className="title" onClick={onMenuClick}>
+        <svg
+          className="logo"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+          />
+        </svg>
+        <h5>{title}</h5>
+      </div>
+
       <div className="profile">
         <img className="picture" src={user.imageUrl} alt="Profile" />
         <p className="name">{user.name}</p>
