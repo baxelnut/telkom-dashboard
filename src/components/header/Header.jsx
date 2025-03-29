bb1b385f07df4756a1bac2d02c69374fc4c89530
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-29 08:34:23 +0700

    refactor header component for improved layout and responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,20 +7,7 @@ export default function Header({ title, user, onMenuClick }) {
 
   return (
     <div className="header-container">
-      <div className="title" onClick={onMenuClick}>
-        <svg
-          className="logo"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-          />
-        </svg>
+      <div className="title">
         <h5>{title}</h5>
       </div>
 
@@ -36,6 +23,21 @@ export default function Header({ title, user, onMenuClick }) {
           <path
             fillRule="evenodd"
             d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+          />
+        </svg>
+
+        <svg
+          className="logo"
+          onClick={onMenuClick}
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
           />
         </svg>
       </div>
