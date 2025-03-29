d53626a4dbd46265cc4c67419f294b0d38fce543
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-29 09:10:18 +0700

    refactor performance visualization components for improved layout and responsiveness

--- src/features/performance/PerformanceVGraphContent.jsx
+++ src/features/performance/PerformanceVGraphContent.jsx
@@ -4,9 +4,12 @@ import "./PerformanceVGraphContent.css";
 export default function PerformanceVGraphContent() {
   return (
     <div className="v-graph-container">
-      <div className="v-graph-content">
+      <div className="head">
         <h5>Additional Insights</h5>
         <p>Graphs to showcase trends and patterns.</p>
+      </div>
+
+      <div className="v-graph-content">
         <pre>Insert content here...</pre>
       </div>
     </div>
