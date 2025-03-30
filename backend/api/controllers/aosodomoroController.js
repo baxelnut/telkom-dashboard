f17b8856f15592e9412a165187be0b3717e62f8f
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-30 16:01:19 +0700

    refactor: limit results in getAllAosodomoro API endpoint to improve performance

--- backend/api/controllers/aosodomoroController.js
+++ backend/api/controllers/aosodomoroController.js
@@ -2,7 +2,10 @@ import supabase from "../services/supabaseService.js";
 
 export const getAllAosodomoro = async (req, res) => {
   try {
-    let { data, error } = await supabase.from("aosodomoro").select("*");
+    let { data, error } = await supabase
+      .from("aosodomoro")
+      .select("*")
+      .limit(11);
 
     if (error) throw error;
     res.json(data);
