2971b08dd0237ae3913fdda6dc1b2b0d450c2632
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-30 16:01:07 +0700

    refactor: add example route for testing and improve server startup logging

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
 import aosodomoroRoutes from "./routes/aosodomoro.js";
+import helloExample from "./example/hello.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
+// Fetch all AOSODOMORO table
 app.use("/api/aosodomoro", aosodomoroRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// test
+app.use("/api/hello", helloExample);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
