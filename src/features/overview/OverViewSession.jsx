a09d849098931bf0da6f9689276beae46b90a33b
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-01 05:38:17 +0700

    feat: enhance OverViewSession component with loading/error states and aggregate session data display

--- src/features/overview/OverViewSession.jsx
+++ src/features/overview/OverViewSession.jsx
@@ -1,32 +1,83 @@
 import React from "react";
 import "./OverViewSession.css";
+import Loading from "../../components/utils/Loading";
+import Error from "../../components/utils/Error";
 
-export default function OverViewSession({ title, subtitle, overviewSession }) {
+export default function OverViewSession({
+  title,
+  subtitle,
+  overviewSession,
+  loading,
+  error,
+}) {
+  const keys = [
+    "disconnect",
+    "suspend",
+    "renewal_agreement",
+    "modify",
+    "resume",
+    "modify_price",
+    "modify_ba",
+    "modify_termin",
+  ];
+
+  const aggregateData = () => {
+    return overviewSession.reduce((aggregated, customer) => {
+      keys.forEach((key) => {
+        aggregated[key] = (aggregated[key] || 0) + (customer[key] || 0);
+      });
+      return aggregated;
+    }, {});
+  };
+
+  const aggregatedData = aggregateData();
+  const total = Object.values(aggregatedData).reduce(
+    (sum, value) => sum + value,
+    0
+  );
+
+  const displayData = keys.map((key) => ({
+    displayName: key,
+    sessions: aggregatedData[key],
+    percentage: ((aggregatedData[key] / total) * 100).toFixed(2),
+  }));
+
   return (
     <div className="session-container">
       <div className="session-title">
         <h4>{title}</h4>
         <p>{subtitle}</p>
       </div>
-      
-      <div className="session-content">
-        {overviewSession.map((customer, index) => (
-          <div className="widget" key={index}>
-            <div className="head">
-              <h6 title={customer.name}>{customer.name}</h6>
 
-              <h6>
-                {customer.sessions} • {customer.percentage.toFixed(2)}%
-              </h6>
+      <div className="session-content">
+        {loading || error ? (
+          loading ? (
+            <Loading />
+          ) : (
+            <Error />
+          )
+        ) : (
+          displayData.map((display, index) => (
+            <div className="widget" key={index}>
+              <div className="head">
+                <h6 className="title">
+                  {/* Convert snake_case to Capitalize Each Word */}
+                  {display.displayName
+                    .replace(/_/g, " ")
+                    .replace(/\b\w/g, (char) => char.toUpperCase())}
+                </h6>
+                <p>{display.sessions}</p>
+                <h6>• {display.percentage}%</h6>
+              </div>
+              <div className="session-bar-graph">
+                <div
+                  className="session-bar-active"
+                  style={{ width: `${display.percentage}%` }}
+                ></div>
+              </div>
             </div>
-            <div className="session-bar-graph">
-              <div
-                className="session-bar-active"
-                style={{ width: `${customer.percentage}%` }}
-              ></div>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
