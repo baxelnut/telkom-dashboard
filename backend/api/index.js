ede42464323efda865853ea8f8e9654753311b6e
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-11 15:53:33 +0700

    feat: add export_to_sheet route for exporting data to Google Sheets

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 import aosodomoroRoutes from "./routes/aosodomoro.js";
 import regional3Routes from "./routes/regional.js";
-import updateReg3Data from "./routes/regional.js";
 import helloExample from "./example/hello.js";
+import exportToSheet from "./routes/exportToSheet.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,6 +19,8 @@ app.use("/api/aosodomoro", aosodomoroRoutes);
 
 // Fetch regional_3 table
 app.use("/api/regional_3", regional3Routes);
+
+app.use("/api/export_to_sheet", exportToSheet);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
