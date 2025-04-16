fb850ff291f55a1706e3ee11d8021b906844773c
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-16 10:54:44 +0700

    feat: implement UserProfile component with dropdown overlay and user information display

---
+++ src/components/header/UserProfile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./UserProfile.css";
+
+export default function UserProfile({ user, toggleDropdown }) {
+  return (
+    <>
+      <div className="dropdown-overlay" onClick={toggleDropdown}></div>
+      <div className="dropdown-user">
+        <div className="close-btn" onClick={toggleDropdown}>
+          <h6>✕</h6>
+        </div>
+
+        <img src={user.imageUrl} />
+
+        <div className="user-section">
+          <p className="label">Username</p>
+          <div className="user-info">{user.name}</div>
+        </div>
+
+        <div className="user-section">
+          <p className="label">Email</p>
+          <div className="user-info">{user.email}</div>
+        </div>
+
+        <button className="edit-profile-btn">
+          <p>Edit profile</p>
+        </button>
+        <button className="change-pw-btn">
+          <p>Change password</p>
+        </button>
+
+        <button
+          className="sign-out-btn"
+          onClick={() => {
+            import("firebase/auth").then(({ getAuth, signOut }) => {
+              signOut(getAuth());
+            });
+          }}
+        >
+          <p>Sign out</p>
+        </button>
+      </div>
+    </>
+  );
+}
