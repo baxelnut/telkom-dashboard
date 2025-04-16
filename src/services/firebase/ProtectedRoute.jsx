f7a6baffabc2ab6aef596b9e99420c61697df2be
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-16 10:54:47 +0700

    fix: replace loading message with Loading component for better user experience

--- src/services/firebase/ProtectedRoute.jsx
+++ src/services/firebase/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import Loading from "../../components/utils/Loading";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <Loading backgroundColor="transparent" />;
   if (!user) return <Navigate to="/login" replace />;
 
   return children;
