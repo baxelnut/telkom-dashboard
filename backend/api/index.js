0dd3f2a383a61d34b5b99ad1d7209a5883dafa62
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-21 11:43:24 +0700

    fix: remove commented-out code for clarity in index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,13 +11,8 @@ app.use(express.json({ limit: "20mb" }));
 
 const PORT = process.env.PORT || 5000;
 
-// test
 app.use("/api/hello", helloExample);
-
-// Fetch all AOSODOMORO table
 app.use("/api/aosodomoro", aosodomoroRoutes);
-
-// Fetch regional_3 table
 app.use("/api/regional_3", regional3Routes);
 
 app.use("/api/export_to_sheet", exportToSheet);
