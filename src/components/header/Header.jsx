c9cc9436456d804a7296da63c53ec539e848edf7
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-24 10:11:07 +0700

    and sun and moon on the left side of the dishtinguished gentleman's image to act as toggle button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 import UserProfile from "./UserProfile";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export default function Header({
   title,
@@ -8,10 +9,24 @@ export default function Header({
   onMenuClick,
   showDropdown,
   setShowDropdown,
+  isDarkMode,
+  setIsDarkMode,
 }) {
   const toggleDropdown = () => {
     setShowDropdown((prev) => !prev);
   };
+
+  const toggleDarkMode = (checked) => {
+    setIsDarkMode(checked);
+  };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkMode]);
 
   return (
     <div className="header-container">
@@ -20,6 +35,14 @@ export default function Header({
       </div>
 
       <div className="profile">
+        <div className="toggle-mode">
+          <DarkModeSwitch
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+            size={20}
+          />
+        </div>
+
         <img
           className="picture"
           src={user.imageUrl ?? "/images/default_profile.png"}
@@ -27,20 +50,17 @@ export default function Header({
           referrerPolicy="no-referrer"
         />
 
-        <p className="name">{user.name ?? user.email ?? "No email? :("}</p>
+        <p className="name">{user.name ?? user.email ?? "..."}</p>
 
         <svg
+          className="chevron-down"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
-          className="chevron-down"
           viewBox="0 0 16 16"
-          style={{ cursor: "pointer" }}
           onClick={toggleDropdown}
+          style={{ cursor: "pointer" }}
         >
-          <path
-            fillRule="evenodd"
-            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-          />
+          <path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
         </svg>
 
         <svg
@@ -52,10 +72,7 @@ export default function Header({
           fill="currentColor"
           viewBox="0 0 16 16"
         >
-          <path
-            fillRule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-          />
+          <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
         </svg>
 
         {showDropdown && (
