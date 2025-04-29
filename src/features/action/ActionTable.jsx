890764549ca7fcb19b22ead09541eeba9dd3fb48
Basilius Tengang <basiliustengang24@gmail.com>
2025-04-29 17:00:40 +0700

    refactor ActionTable component to improve readability and update prop naming

--- src/features/action/ActionTable.jsx
+++ src/features/action/ActionTable.jsx
@@ -4,22 +4,40 @@ import Loading from "../../components/utils/Loading";
 import Error from "../../components/utils/Error";
 
 export default function ActionTable({
-  reportTableData,
+  actionTabledata,
   selectedCategory,
   orderSubtypes,
   loading,
   error,
   onCellSelect,
 }) {
-  if (loading) {
-    return <Loading backgroundColor="transparent" />;
-  }
+  if (loading) return <Loading backgroundColor="transparent" />;
+  if (error) return <Error message={error} />;
 
-  if (error) {
-    return <Error message={error} />;
-  }
+  const dataArray = actionTabledata?.data ?? [];
+  const headers = dataArray.length > 0 ? Object.keys(dataArray[0]) : [];
 
-  return <div className="action-table">
-    a
-  </div>;
+  return (
+    <div className="action-table">
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header, i) => (
+              <th key={i}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {dataArray.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {headers.map((header, colIndex) => (
+                <td key={colIndex}>{row[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
