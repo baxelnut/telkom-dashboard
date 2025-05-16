4800a948c317600943518785fed1875eabf98ccb
Basilius Tengang <basiliustengang24@gmail.com>
2025-05-16 09:52:39 +0700

    feat: update firebaseAdmin to use environment variable for service account in production

--- backend/api/firebaseAdmin.js
+++ backend/api/firebaseAdmin.js
@@ -1,8 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "../keys/serviceAccountKey.json" assert { type: "json" };
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// import serviceAccount from "../keys/serviceAccountKey.json" assert { type: "json" }; // IN DEVELOPMENT
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON); // IN PRODUCTION
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export const db = admin.firestore();
