2b1cfac8236199d41d41f70a22d939203b228092
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-11 23:36:56 +0700

    new routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,8 @@
 // Pages
+import ActionBasedPage from "../pages/action-based/ActionBasedPage";
+import AdminPanelPage from "../pages/admin-panel/AdminPanelPage";
+import AosodomoroReportPage from "../pages/reports/aosodomoro/AosodomoroReportPage";
+import GalaksiReportPage from "../pages/reports/galaksi/GalaksiReportPage";
 import OverviewPage from "../pages/overview/OverviewPage";
 // Routes
 import { ProtectedRoute, RedirectIfLoggedIn } from "./ProtectedRoute";
@@ -6,4 +10,8 @@ import { ProtectedRoute, RedirectIfLoggedIn } from "./ProtectedRoute";
 export const appRoutes = [
   { path: "/", element: <OverviewPage /> },
   { path: "/overview", element: <OverviewPage /> },
+  { path: "/reports/aosodomoro", element: <AosodomoroReportPage /> },
+  { path: "/reports/galaksi", element: <GalaksiReportPage /> },
+  { path: "/action-based", element: <ActionBasedPage /> },
+  { path: "/admin-panel", element: <AdminPanelPage /> },
 ];
