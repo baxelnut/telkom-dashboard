e715f1385815bad23008e1a3a0e978d59bada1e4
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-11 23:37:10 +0700

    page not found made prettier

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 // Style
 import "./PageNotFound.css";
+import Button from "../components/ui/buttons/Button";
 
 export default function PageNotFound() {
   return (
     <div className="page-not-found">
-      <h1>404</h1>
-      <h5>Page Not Found</h5>
-      <Link to="/overview">
-        <p>Back to Dashboard</p>
-      </Link>
+      <h1 className="title">Oops!</h1>
+      <h5>404 | Page Not Found</h5>
+      <p>We can't seem to find the page you are looking for!</p>
+      <Button text="Back to Dashboard" arrow to="/overview" />
     </div>
   );
 }
