a1c22f3d1dd0d2c8a05ccd9bb68935c7438f3454
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-13 12:37:57 +0700

    fix style for error msg

--- src/components/ui/states/Error.jsx
+++ src/components/ui/states/Error.jsx
@@ -5,11 +5,11 @@ import Icon from "../icons/Icon";
 // Data
 import { SVG_PATHS } from "../../../data/utilData";
 
-export default function Error({ message }) {
+export default function Error({ message, className = "error-container" }) {
   const displayMessage = message?.trim() ? message : "Something went wrong!";
 
   return (
-    <div className="error-container">
+    <div className={className}>
       <div className="error-icon">
         <Icon path={SVG_PATHS.error} />
       </div>
