e9120863e90e41f69f4e6d08629d6433782689cb
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-13 20:55:25 +0700

    refactoring app to be more efficient and reduce redundant, fixing the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {
   Navigate,
   BrowserRouter as Router,
@@ -8,24 +7,17 @@ import {
 // Style
 import "./App.css";
 // Components
-import Error from "./components/ui/states/Error";
-import Loading from "./components/ui/states/Loading";
 import ScrollToTop from "./components/utils/ScrollToTop";
 import PageNotFound from "./pages/PageNotFound";
 // Layouts
 import Layout from "./components/layouts/Layout";
 // Context
-import { useAuth } from "./context/AuthContext";
 import { useTheme } from "./context/ThemeContext";
 // Routes
 import { appRoutes } from "./routes/AppRoutes";
 import LoginPage from "./pages/auth/LoginPage";
 
-const API_URL = import.meta.env.VITE_API_URL;
-const DEV_API_URL = import.meta.env.VITE_DEV_API;
-
 export default function App() {
-  const { user } = useAuth();
   const { isDarkMode } = useTheme();
 
   return (
