2e9dda149474c8abe4a0d0a11d590d5b4b484333
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-13 14:57:11 +0700

    fix the button style and icon positioning

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -9,7 +9,7 @@ export default function PageNotFound() {
       <h1 className="title">Oops!</h1>
       <h5>404 | Page Not Found</h5>
       <p>We can't seem to find the page you are looking for!</p>
-      <Button text="Back to Dashboard" arrow to="/overview" />
+      <Button text="Back to Dashboard" to="/overview" arrow iconAfter />
     </div>
   );
 }
