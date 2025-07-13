ad803ce6648a1ca8f1f5e3823e9edd55c0730c30
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-13 20:57:07 +0700

    adding API URLs to app routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,24 +6,39 @@ import GalaksiReportPage from "../pages/reports/galaksi/GalaksiReportPage";
 import OverviewPage from "../pages/overview/OverviewPage";
 // Routes
 import { ProtectedRoute, RedirectIfLoggedIn } from "./ProtectedRoute";
+// API URLs
+const API_URL = import.meta.env.VITE_API_URL;
+const DEV_API_URL = import.meta.env.VITE_DEV_API;
 
 export const appRoutes = [
-  { path: "/", element: <OverviewPage />, title: "Overview" },
-  { path: "/overview", element: <OverviewPage />, title: "Overview" },
+  {
+    path: "/",
+    element: <OverviewPage API_URL={DEV_API_URL} />,
+    title: "Overview",
+  },
+  {
+    path: "/overview",
+    element: <OverviewPage API_URL={DEV_API_URL} />,
+    title: "Overview",
+  },
   {
     path: "/reports/aosodomoro",
-    element: <AosodomoroReportPage />,
+    element: <AosodomoroReportPage API_URL={DEV_API_URL} />,
     title: "Report AOSODOMORO",
   },
   {
     path: "/reports/galaksi",
-    element: <GalaksiReportPage />,
+    element: <GalaksiReportPage API_URL={DEV_API_URL} />,
     title: "Report GALAKSI",
   },
   {
     path: "/action-based",
-    element: <ActionBasedPage />,
+    element: <ActionBasedPage API_URL={DEV_API_URL} />,
     title: "Action-Based",
   },
-  { path: "/admin-panel", element: <AdminPanelPage />, title: "Admin Panel" },
+  {
+    path: "/admin-panel",
+    element: <AdminPanelPage API_URL={DEV_API_URL} />,
+    title: "Admin Panel",
+  },
 ];
