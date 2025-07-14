bea30983d74572b349392e50c2f6dff9644412e3
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-14 22:33:06 +0700

    protect route, only logged user can access, make loading state more pretty

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,23 +1,40 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+// Components
+import Loading from "../components/ui/states/Loading";
 
 export function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-  if (user === undefined) return null;
-  if (!user) return <Navigate to="/login" replace />;
+  const { user, authLoading, isApprovedUser } = useAuth();
+  if (authLoading)
+    return (
+      <div className="app-container">
+        <Loading backgroundColor="transparent" />
+      </div>
+    );
+  if (!user || !isApprovedUser) return <Navigate to="/login" replace />;
   return children;
 }
 
 export function RedirectIfLoggedIn({ children }) {
-  const { user } = useAuth();
-  if (user === undefined) return null;
-  if (user) return <Navigate to="/overview" replace />;
+  const { user, authLoading, isApprovedUser } = useAuth();
+  if (authLoading)
+    return (
+      <div className="app-container">
+        <Loading backgroundColor="transparent" />
+      </div>
+    );
+  if (user && isApprovedUser) return <Navigate to="/overview" replace />;
   return children;
 }
 
 export function VerifiedRoute({ children }) {
-  const { user } = useAuth();
-  if (user === undefined) return null;
+  const { user, authLoading } = useAuth();
+  if (authLoading)
+    return (
+      <div className="app-container">
+        <Loading backgroundColor="transparent" />
+      </div>
+    );
   if (!user) return <Navigate to="/login" replace />;
   if (!user.emailVerified) return <Navigate to="/verify-email" replace />;
   return children;
