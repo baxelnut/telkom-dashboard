0388dfd7a18d194a8da9d77d98a0dbf22fe8e491
Basilius Tengang <basiliustengang24@gmail.com>
2025-07-14 21:01:40 +0700

    make the parent class for overview page only ui, keep it clean and dumb

--- src/pages/overview/OverviewPage.jsx
+++ src/pages/overview/OverviewPage.jsx
@@ -2,32 +2,32 @@
 import "./OverviewPage.css";
 // Cards
 import AmmountBySubType from "../../features/overview/AmmountBySubType";
+import CategoryByWitel from "../../features/overview/CategoryByWitel";
+import DataPreview from "../../features/overview/DataPreview";
 import OverviewByWitel from "../../features/overview/OverviewByWitel";
 import RevBySubType from "../../features/overview/RevBySubType";
+import SessionBySubType from "../../features/overview/SessionBySubType";
+import SegmenRadar from "../../features/overview/SegmenRadar";
 
 export default function OverviewPage({ API_URL }) {
   return (
     <div className="overview-page">
       <OverviewByWitel API_URL={API_URL} />
-      
+
       <div className="cards-container-row-1-1">
         <RevBySubType API_URL={API_URL} />
         <AmmountBySubType API_URL={API_URL} />
       </div>
 
       <div className="cards-container-row">
-        <div className="card session-by-sub-type">
-          <h6>Session by Sub-type</h6>
-        </div>
+        <SessionBySubType API_URL={API_URL} />
         <div className="card segmen-radar-chart">
-          <h6>Segmen Radar Chart</h6>
-          <h6>Witel Pie Chart</h6>
+          <SegmenRadar API_URL={API_URL} />
+          <CategoryByWitel API_URL={API_URL} />
         </div>
       </div>
 
-      <div className="card data-overview">
-        <h6>Data Overview</h6>
-      </div>
+      <DataPreview API_URL={API_URL} />
     </div>
   );
 }
