06e831286ec3e7bb3c4e81bc807d5bd3b288b4ea
Basilius Tengang <basiliustengang24@gmail.com>
2025-08-12 11:52:48 +0700

    refactor table actuon header for better readablity

---
+++ src/pages/action-based/ActionTableHeader.jsx
@@ -0,0 +1,75 @@
+// Style
+import "./ActionTableHeader.css";
+// Components
+import Button from "../../components/ui/buttons/Button";
+import Dropdown from "../../components/ui/input/Dropdown";
+// Helpers
+import { getExportOptions } from "../../helpers/exportHelpers";
+
+export default function ActionTableHeader({
+  witel,
+  witelOptions,
+  selected,
+  setSelected,
+  handleViewFull,
+  handleExport,
+  po,
+  status,
+  period,
+  bucketLabel,
+}) {
+  return (
+    <div className="action-table-header">
+      {!witel ? (
+        <div className="filter-container">
+          <h5>{bucketLabel}</h5>
+          <div className="filter-items">
+            <p>Select witel:</p>
+            <Dropdown
+              options={witelOptions}
+              value={selected.witel}
+              onChange={(e) =>
+                setSelected((prev) => ({
+                  ...prev,
+                  witel: e.target.value,
+                }))
+              }
+              short
+              chevronDown
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="filter-container back-btn">
+          <div className="filter-items">
+            <Button text="Back" onClick={handleViewFull} arrowLeft short />
+          </div>
+          <div className="filter-items">
+            <Button text="Export as" onClick={handleExport} short />
+            <Dropdown
+              options={getExportOptions()}
+              value={selected.exportType}
+              onChange={(e) =>
+                setSelected((prev) => ({
+                  ...prev,
+                  exportType: e.target.value,
+                }))
+              }
+              short
+              chevronDown
+            />
+          </div>
+        </div>
+      )}
+
+      {po && (
+        <div className="title">
+          <h6>
+            {po} → {status} →{" "}
+            {period === "ALL PERIOD" ? period : `${period} bulan`}
+          </h6>
+        </div>
+      )}
+    </div>
+  );
+}
