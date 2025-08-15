1bf1f6a0212da248dab5f81b83d7d7d79df4c231
Basilius Tengang <basiliustengang24@gmail.com>
2025-08-15 08:59:52 +0700

    added reset button manual send by admin

--- src/features/admin/RolePopUpMenu.jsx
+++ src/features/admin/RolePopUpMenu.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 // Style
 import "./RolePopUpMenu.css";
 // Components
 import Button from "../../components/ui/buttons/Button";
+// Custom hook
+import { useEmailAuth } from "../../hooks/useEmailAuth";
 // Data
 import { SVG_PATHS } from "../../data/utilsData";
 
@@ -16,6 +19,14 @@ export default function RolePopUpMenu({
   handleDelete,
 }) {
   const navigate = useNavigate();
+  const { forgotPassword, message } = useEmailAuth({});
+  const [resetDisabled, setResetDisabled] = useState(false);
+
+  const handleResetClick = async () => {
+    await forgotPassword(email);
+    setResetDisabled(true);
+    setTimeout(() => setResetDisabled(false), 60 * 1000); // 1 min cooldown
+  };
 
   return (
     <div className="popup-menu" ref={menuRef}>
@@ -45,6 +56,16 @@ export default function RolePopUpMenu({
         fullWidth
       />
       <Button
+        text={resetDisabled ? "Sent to Email" : "Reset Password"}
+        iconPath={SVG_PATHS.emailWarning}
+        onClick={handleResetClick}
+        backgroundColor="var(--text)"
+        textColor="var(--bg)"
+        short
+        fullWidth
+        disabled={resetDisabled}
+      />
+      <Button
         text="Delete"
         iconPath={SVG_PATHS.trash}
         onClick={() => handleDelete(uid)}
@@ -52,6 +73,9 @@ export default function RolePopUpMenu({
         short
         fullWidth
       />
+      {message?.text && (
+        <p className={`message ${message.type}`}>{message.text}</p>
+      )}
     </div>
   );
 }
