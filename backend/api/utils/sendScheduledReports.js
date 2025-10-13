e107bae49659c495e515f4c9b9efd8acd4523d25
Basilius Tengang <basiliustengang24@gmail.com>
2025-10-13 12:41:26 +0700

    temp Friday (5)

--- backend/api/utils/sendScheduledReports.js
+++ backend/api/utils/sendScheduledReports.js
@@ -15,7 +15,7 @@ const now = new Date();
 const utcHour = now.getUTCHours();
 const utcDay = now.getUTCDay();
 
-const isScheduledDay = utcDay === 1; // Monday (1)
+const isScheduledDay = utcDay === 5; // temp Friday (5)
 const isInTimeWindow = utcHour >= 6 && utcHour < 11; // 06:00-11:00 UTC (13:00-18:00 WIB)
 if (!(isScheduledDay && isInTimeWindow)) {
   console.log(
